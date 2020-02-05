6991217c4212896d52569e646c1250f5c6ba66b1
Harry Cai <harry.cai@hotmail.com>
2020-02-05 00:46:05 +0000

    I'm at my wit's end

--- test/TestCryptomon.sol
+++ test/TestCryptomon.sol
@@ -13,111 +13,111 @@ import "../contracts/Cryptomon.sol";
 
 contract TestCryptomon {
 
-    Cryptomon public mon;
-    address payable testCreator;
+    // Cryptomon public mon;
+    // address payable testCreator;
 
-    uint public initialBalance = 1 ether;
-    event FundsReceived(address from, uint amount);
-    receive () external payable {
-        emit FundsReceived(msg.sender, msg.value);
-    }
+    // uint public initialBalance = 1 ether;
+    // event FundsReceived(address from, uint amount);
+    // receive () external payable {
+    //     emit FundsReceived(msg.sender, msg.value);
+    // }
 
-    constructor() public {
-        testCreator = msg.sender;
-    }
+    // constructor() public {
+    //     testCreator = msg.sender;
+    // }
 
-    modifier asAdmin() {
-        mon = new Cryptomon();
+    // modifier asAdmin() {
+    //     mon = new Cryptomon();
 
-        require(mon._admin() == address(this), "Should run this test as admin");
-        _;
-    }
+    //     require(mon._admin() == address(this), "Should run this test as admin");
+    //     _;
+    // }
 
-    modifier asNormalUser() {
-        mon = Cryptomon(DeployedAddresses.Cryptomon());
-        require(mon._admin() != address(this), "Should run this test as not an admin");
-        _;
-    }
+    // modifier asNormalUser() {
+    //     mon = Cryptomon(DeployedAddresses.Cryptomon());
+    //     require(mon._admin() != address(this), "Should run this test as not an admin");
+    //     _;
+    // }
 
-    function testAddInitialPokemon() public asAdmin() {
-        uint numAdded = mon.addInitialPokemon();
-        Assert.isTrue(numAdded > 0, "Failed to add any initial pokemon");
-        Assert.isTrue(mon._totalNumPokemon() > 0, "Claimed to, but failed to, add any initial pokemon");
-    }
+    // function testAddInitialPokemon() public asAdmin() {
+    //     uint numAdded = mon.addInitialPokemon();
+    //     Assert.isTrue(numAdded > 0, "Failed to add any initial pokemon");
+    //     Assert.isTrue(mon._totalNumPokemon() > 0, "Claimed to, but failed to, add any initial pokemon");
+    // }
 
-    function testDefineSpeciesAddPokemon() public asAdmin() {
-        mon.defineSpecies("Mimikyu", "Dark", "Mimikyu", "Mimikyu", 2);
-        Assert.equal(mon._speciesType("Mimikyu"), "Dark", "Species type not set correctly");
-        Assert.equal(mon._speciesEvolvesTo("Mimikyu"), "Mimikyu", "Species evolution not set correctly");
-        Assert.equal(mon._speciesBreedsTo("Mimikyu"), "Mimikyu", "Species breeding not set correctly");
-        Assert.equal(uint(mon._speciesTimesCanBreed("Mimikyu")), 2, "Species times can breed not set correctly");
+    // function testDefineSpeciesAddPokemon() public asAdmin() {
+    //     mon.defineSpecies("Mimikyu", "Dark", "Mimikyu", "Mimikyu", 2);
+    //     Assert.equal(mon._speciesType("Mimikyu"), "Dark", "Species type not set correctly");
+    //     Assert.equal(mon._speciesEvolvesTo("Mimikyu"), "Mimikyu", "Species evolution not set correctly");
+    //     Assert.equal(mon._speciesBreedsTo("Mimikyu"), "Mimikyu", "Species breeding not set correctly");
+    //     Assert.equal(uint(mon._speciesTimesCanBreed("Mimikyu")), 2, "Species times can breed not set correctly");
         
         
-        uint id = mon.addPokemon(
-            "Mimikyu",
-            0.1 ether,
-            true,
-            3
-        );
+    //     uint id = mon.addPokemon(
+    //         "Mimikyu",
+    //         100 finney,
+    //         true,
+    //         3
+    //     );
 
-        address newPokemonOwner = mon._pokOwner(id);
-        Assert.equal(newPokemonOwner, mon._admin(), "Newly created Pokemon should be owned by admin");
-    }
-
-    function testBuyPokemon() public asNormalUser() {
-        uint idToBuy = 1;
-        uint price = mon._pokPrice(idToBuy);
-        uint id = mon.buyPokemon.value(price)(idToBuy);
-        Assert.equal(id, idToBuy, "buyPokemon returned different id to what was requested");
-        Assert.equal(mon._pokOwner(id), address(this), "This contract should own the pokemon after buying");
-    }
-
-    function testSellPokemon() public asNormalUser() {
-        uint id = 1;
-        uint price = 2 ether;
-        mon.sellPokemon(id, price);
-        Assert.isTrue(mon._pokForSale(id), "The pokemon wasn't listed for sale");
-        Assert.equal(mon._pokPrice(id), price, "The pokemon wasn't listed at the correct price");
-    }
-
-    function testFight() public asNormalUser() {
-        // TODO: pick uneven fights with certain outcomes, then test winner and loser on evolve and revive
-        uint id = 1;
-        uint lvlBefore = mon._pokLevel(id);
-        bool win = mon.fight(id, 0);
-        if (win) {
-            Assert.equal(uint(mon._pokLevel(id)), lvlBefore+1, "Pokemon won but didn't gain a level");
-        } else {
-            Assert.isTrue(mon._pokStunned(id), "Pokemon lost but didn't get stunned");
-        }
-    }
-
-    // function testEvolve() public asNormalUser() {
-    //     // TODO
+    //     address newPokemonOwner = mon._pokOwner(id);
+    //     Assert.equal(newPokemonOwner, mon._admin(), "Newly created Pokemon should be owned by admin");
     // }
 
-    // function testRevive() public asNormalUser() {
-    //     // TODO
+    // function testBuyPokemon() public asNormalUser() {
+    //     uint idToBuy = 1;
+    //     uint price = mon._pokPrice(idToBuy);
+    //     uint id = mon.buyPokemon.value(price)(idToBuy);
+    //     Assert.equal(id, idToBuy, "buyPokemon returned different id to what was requested");
+    //     Assert.equal(mon._pokOwner(id), address(this), "This contract should own the pokemon after buying");
     // }
 
-    // function testBreed() public asNormalUser() {
-    //     // TODO
+    // function testSellPokemon() public asNormalUser() {
+    //     uint id = 1;
+    //     uint price = 2 ether;
+    //     mon.sellPokemon(id, price);
+    //     Assert.isTrue(mon._pokForSale(id), "The pokemon wasn't listed for sale");
+    //     Assert.equal(mon._pokPrice(id), price, "The pokemon wasn't listed at the correct price");
     // }
 
-    function testWithdrawFunds() public asNormalUser() {
-        address(mon).transfer(0.1 ether);
-        uint pendingBalance = mon.checkBalance();
-        Assert.isTrue(pendingBalance > 0, "Zero balance in pending withdrawals");
-        uint balanceBefore = address(this).balance;
-        uint amount = mon.withdrawFunds();
-        Assert.equal(amount, pendingBalance, "Returned amount doesn't match pending balance");
-        uint balanceAfter = address(this).balance;
-        Assert.equal(amount, balanceAfter-balanceBefore, "Incorrect amount transferred");
-    }
+    // function testFight() public asNormalUser() {
+    //     // TODO: pick uneven fights with certain outcomes, then test winner and loser on evolve and revive
+    //     uint id = 1;
+    //     uint lvlBefore = mon._pokLevel(id);
+    //     bool win = mon.fight(id, 0);
+    //     if (win) {
+    //         Assert.equal(uint(mon._pokLevel(id)), lvlBefore+1, "Pokemon won but didn't gain a level");
+    //     } else {
+    //         Assert.isTrue(mon._pokStunned(id), "Pokemon lost but didn't get stunned");
+    //     }
+    // }
 
-    function afterAll() public {
-        testCreator.transfer(address(this).balance);
-    }
+    // // function testEvolve() public asNormalUser() {
+    // //     // TODO
+    // // }
+
+    // // function testRevive() public asNormalUser() {
+    // //     // TODO
+    // // }
+
+    // // function testBreed() public asNormalUser() {
+    // //     // TODO
+    // // }
+
+    // function testWithdrawFunds() public asNormalUser() {
+    //     address(mon).transfer(100 finney);
+    //     uint pendingBalance = mon.checkBalance();
+    //     Assert.isTrue(pendingBalance > 0, "Zero balance in pending withdrawals");
+    //     uint balanceBefore = address(this).balance;
+    //     uint amount = mon.withdrawFunds();
+    //     Assert.equal(amount, pendingBalance, "Returned amount doesn't match pending balance");
+    //     uint balanceAfter = address(this).balance;
+    //     Assert.equal(amount, balanceAfter-balanceBefore, "Incorrect amount transferred");
+    // }
+
+    // function afterAll() public {
+    //     testCreator.transfer(address(this).balance);
+    // }
 
 
 }
--- test/cryptomon.js
+++ test/cryptomon.js
@@ -1,12 +1,14 @@
 const Cryptomon = artifacts.require("Cryptomon");
 
 contract("Cryptomon", accounts => {
-    it("test constructor", async () => {
+    it("int test, admin setup", async () => {
         let contract = await Cryptomon.deployed();
         let owner = await contract._admin.call();
         assert.equal(owner, accounts[0]);
 
+        let numAdded = await contract.addInitialPokemon();
         let numPokemon = await contract._totalNumPokemon.call();
+        assert.equal(numAdded, numPokemon);
         assert.equal(numPokemon, 2);
 
         let firstPokemonSpecies = web3.utils.hexToUtf8(
@@ -14,54 +16,60 @@ contract("Cryptomon", accounts => {
         assert.equal(firstPokemonSpecies, "Tapu Koko");
     });
 
-    it("test addPokemon", async () => {
+    it("int test, admin adding new species and cards", async () => {
         let contract = await Cryptomon.deployed();
-        let result = await contract.addPokemon.call(
+        let result1 = await contract.defineSpecies.call(
             web3.utils.utf8ToHex("Mimikyu"),    // species
             web3.utils.utf8ToHex("Dark"),       // type
-            web3.utils.toWei('1', "ether"),     // price
-            3,                                  // level
             web3.utils.utf8ToHex("Mimikyu"),    // evolvesTo
-            2,                                  // timesCanBreed
             web3.utils.utf8ToHex("Mimikyu"),    // breedsTo
+            2,                                  // timesCanBreed
+            { from: accounts[0] }
+        )
+        let result2 = await contract.addPokemon.call(
+            web3.utils.utf8ToHex("Mimikyu"),    // species
+            web3.utils.toWei('0.1', "ether"),   // price
+            true,                               // forSale
+            3,                                  // level
             { from: accounts[0] }
         );
-        let id = result.toNumber();
+        let id = result2.toNumber();
         assert.equal(id, 2);
-        // TODO: these cause an invalid opcode error. Why?
         let newPokemonOwner = debug(await contract.getOwner.call(id));
         assert.equal(newPokemonOwner, accounts[0]);
         let newPokemonSpecies = await contract._pokSpecies.call(2);
-        assert.equal(newPokemonSpecies, "Tapu Koko");
+        assert.equal(newPokemonSpecies, "Mimikyu");
     });
 
-    // it("test getIdsForSale", async () => {
-    //     let contract = await Cryptomon.deployed();
-    // });
-
-    // it("test getIdsOwnedBy", async () => {
-    //     let contract = await Cryptomon.deployed();
-    // });
-
-    it("test buyPokemon", async () => {
+    it("int test buying and selling pokemon", async () => {
         let contract = await Cryptomon.deployed();
-        let idToBuy = 1;
-        let player1 = accounts[1];
+        let id = 1;
         let price = web3.utils.toWei('1', 'ether');
-        let result = await contract.buyPokemon.call(idToBuy, {from:player1, value:price});
-        let idBought = result.toNumber();
-        assert.equal(idBought, idToBuy);
+        let resultBuy = await contract.buyPokemon.call(id, {from:accounts[1], value:price});
+        let idBought = resultBuy.toNumber();
+        assert.equal(idBought, id);
         let newOwner = await contract._pokOwner.call(idBought);
-        assert.equal(newOwner, player1);
+        assert.equal(newOwner, accounts[1]);
+        
+        let resultSell = await contract.buyPokemon.call(id, {from:accounts[1], value:price});
+        let resultBuy2 = await contract.buyPokemon.call(id, {from:accounts[2], value:price});
+        let newOwner2 = await contract._pokOwner.call(idBought);
+        assert.equal(newOwner, accounts[2]);
     });
 
-    it("test sellPokemon", async () => {
+    it("int test depositing and withdrawing funds", async () => {
         let contract = await Cryptomon.deployed();
-        let player1 = accounts[1];
-    });
+        let balanceBefore = parseInt(await web3.eth.getBalance(accounts[1]));
+        let amount = parseInt(web3.utils.toWei('100', 'finney'));
+        let result = await contract.sendTransaction({from:accounts[1], value:amount});
+        let balanceInContract = parseInt(await contract.checkBalance.call({from:accounts[1]}));
+        assert.equal(balanceInContract, amount);
+        let balanceAfter = parseInt(await web3.eth.getBalance(accounts[1]));
+        assert.isAtLeast(balanceBefore, balanceAfter + amount); // not testing for equality because gas
 
-    it("test withdrawFunds", async () => {
-        let contract = await Cryptomon.deployed();
+        let result2 = await contract.withdrawFunds.call({from: accounts[1]});
+        let balanceFinal = parseInt(await web3.eth.getBalance(accounts[1]));
+        assert.isAtLeast(balanceFinal, balanceAfter);
     });
 
 
