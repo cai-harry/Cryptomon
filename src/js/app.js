d4d2515ae70841f758787a506a67bccd9041b256
Harry Cai <harry.cai@hotmail.com>
2020-02-09 23:33:52 +0000

    Display account number and balance

--- src/js/app.js
+++ src/js/app.js
@@ -20,7 +20,14 @@ App = {
       pokRow.append(pokTemplate.html());
     };
 
-    return await App.initWeb3();
+    await App.initWeb3();
+
+    await App.initContract();
+
+    App.bindEvents();
+
+    // Use our contract to retrieve and mark the purchased Pokemon
+    await App.render();
   },
 
   initWeb3: async function () {
@@ -30,6 +37,7 @@ App = {
       try {
         // Request account access
         await window.ethereum.enable();
+        console.log("Using modern dapp browser (metamask) as web3 provider");
       } catch (error) {
         // User denied account access...
         console.error("User denied account access")
@@ -37,74 +45,87 @@ App = {
     }
     // Legacy dapp browsers...
     else if (window.web3) {
+      console.log("Using legacy dapp browser as web3 provider");
       App.web3Provider = window.web3.currentProvider;
     }
     // If no injected web3 instance is detected, fall back to Ganache
     else {
+      console.log("Using localhost as web3 provider");
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
     }
     web3 = new Web3(App.web3Provider);
-
-    return App.initContract();
   },
 
-  initContract: function () {
-    $.getJSON('Cryptomon.json', function (data) {
-      // Get the necessary contract artifact file and instantiate it with truffle-contract
-      var CryptomonArtifact = data;
-      App.contracts.Cryptomon = TruffleContract(CryptomonArtifact);
+  initContract: async function () {
+    let data = await $.getJSON('Cryptomon.json');
 
-      // Set the provider for our contract
-      App.contracts.Cryptomon.setProvider(App.web3Provider);
+    // Get the necessary contract artifact file and instantiate it with truffle-contract
+    var CryptomonArtifact = data;
+    App.contracts.Cryptomon = TruffleContract(CryptomonArtifact);
 
-      // Use our contract to retrieve and mark the purchased Pokemon
-      return App.markBought();
-    });
-
-    return App.bindEvents();
+    // Set the provider for our contract
+    App.contracts.Cryptomon.setProvider(App.web3Provider);
   },
 
   bindEvents: function () {
     $(document).on('click', '.btn-buy', App.handleBought);
   },
 
-  markBought: function (buyers, account) {
-    var cryptomonInstance;
+  render: async function (buyers, account) {
 
-    web3.eth.getAccounts(function (error, accounts) {
+    let contract = await App.contracts.Cryptomon.deployed();
+
+    // Display account number
+    web3.eth.getAccounts( (error, accounts) =>
+    {
       if (error) {
-        console.log(error);
+        console.error(error);
       }
+      document.getElementById("account-num").textContent = accounts[0];
+    })
 
-      var account = accounts[0];
+    // Display balance owed by contract
+    let balanceOwed = web3.fromWei(await contract.checkBalance.call());
+    document.getElementById("balance").textContent = balanceOwed;
 
-      let contract = App.contracts.Cryptomon.deployed();
 
-      contract.then(function (instance) {
-        return instance.getPrices.call();
-      }).then(function (prices) {
-        for (i = 0; i < prices.length; i++) {
-          let price = web3.fromWei(parseInt(prices[i]), 'ether');
-          console.log(price);
-          let buttonText = "Buy (" + price + " ETH)";
-          $('.panel-pokemon').eq(i).find('button').text(buttonText);
-        }
-      }).catch(function (err) {
-        console.log(err.message);
-      });
+    // old stuff
+    // var cryptomonInstance;
 
-      contract.then(function (instance) {
-        return instance.getOwners.call();
-      }).then(function (owners) {
-        for (i = 0; i < owners.length; i++) {
-          if (owners[i] == account) {
-            $('.panel-pokemon').eq(i).find('button').text("Owned").attr('disabled', true);
-          }
-        }
-      }).catch(function (err) {
-        console.log(err.message);
-      });
-    });
+    // web3.eth.getAccounts(function (error, accounts) {
+    //   if (error) {
+    //     console.log(error);
+    //   }
+
+    //   var account = accounts[0];
+
+    //   let contract = App.contracts.Cryptomon.deployed();
+
+    //   contract.then(function (instance) {
+    //     return instance.getPrices.call();
+    //   }).then(function (prices) {
+    //     for (i = 0; i < prices.length; i++) {
+    //       let price = web3.fromWei(parseInt(prices[i]), 'ether');
+    //       console.log(price);
+    //       let buttonText = "Buy (" + price + " ETH)";
+    //       $('.panel-pokemon').eq(i).find('button').text(buttonText);
+    //     }
+    //   }).catch(function (err) {
+    //     console.log(err.message);
+    //   });
+
+    //   contract.then(function (instance) {
+    //     return instance.getOwners.call();
+    //   }).then(function (owners) {
+    //     for (i = 0; i < owners.length; i++) {
+    //       if (owners[i] == account) {
+    //         $('.panel-pokemon').eq(i).find('button').text("Owned").attr('disabled', true);
+    //       }
+    //     }
+    //   }).catch(function (err) {
+    //     console.log(err.message);
+    //   });
+    // });
 
   },
 
@@ -116,7 +137,7 @@ App = {
 
     var cryptomonInstance;
 
-    web3.eth.getAccounts(function (error, accounts) {
+    web3.eth.getAccounts((error, accounts) => {
       if (error) {
         console.log(error);
       }
